22941c3fbec6c66e5aeb62a834a1a83398999945
Kolcman <m.kolcman@yandex.ru>
2023-11-29 11:23:53 +0300

    Add repository logic

--- app/src/main/java/com/example/composition/domain/usecases/GetGameSettingUseCase.kt
+++ app/src/main/java/com/example/composition/domain/usecases/GetGameSettingUseCase.kt
@@ -1,17 +1,14 @@
 package com.example.composition.domain.usecases
 
-import com.example.composition.domain.entity.Question
+import com.example.composition.domain.entity.GameSettings
+import com.example.composition.domain.entity.Level
 import com.example.composition.domain.repository.GameRepository
 
 class GetGameSettingUseCase(
     private val repository: GameRepository
 ) {
 
-    operator fun invoke(maxSumValue: Int): Question {
-        return repository.generateQuestion(maxSumValue, COUNT_OF_OPTIONS)
-    }
-
-    private companion object {
-        private const val COUNT_OF_OPTIONS = 6
+    operator fun invoke(level: Level): GameSettings {
+        return repository.getGameSettings(level)
     }
 }
